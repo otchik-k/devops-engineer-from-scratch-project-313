da1540481e698855d3c5db7ea026a906c6b0d396
otchikk <kirill@otchik.ru>
2026-06-14 19:55:35 +0300

    Закомменчен DATABASE_URL в config

--- application/config.py
+++ application/config.py
@@ -1,3 +1,3 @@
-DATABASE_URL = "postgresql://hexlet:12345@localhost:5432/stud_db"
+#DATABASE_URL = "postgresql://hexlet:12345@localhost:5432/stud_db"
 WEB_PORT = 8080
 DOMEN_FOR_SHORT_URL = "https://short.io/r/"
